c44979d8b4e120bab06f31595d8f0146e32bb786
Mateusz <matzaj101@gmail.com>
2023-02-04 23:30:55 +0100

    add speakers module

---
+++ src/Modules/Speakers/Confab.Modules.Speakers.Core/DAL/SpeakersDbContext.cs
@@ -0,0 +1,23 @@
+﻿
+using Confab.Modules.Speakers.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Confab.Modules.Speakers.Core.DAL
+{
+    public class SpeakersDbContext : DbContext
+    {
+        public DbSet<Speaker> Speakers { get; set; }
+
+        public SpeakersDbContext(DbContextOptions<SpeakersDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("speakers");
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+    }
+}
